173db027e67ef185560f535df33758bd327b9654
seikoso <75853142+seikosos@users.noreply.github.com>
2025-12-21 14:48:09 +0100

    discord module

---
+++ DiscordActivity.lua
@@ -0,0 +1,88 @@
+local module = {}
+
+local ws = Websocket or WebSocket
+local HttpService = game:GetService("HttpService")
+
+local function Heartbeat(interval)
+	task.spawn(function()
+		while true do
+			task.wait(interval / 1000)
+			connection:Send(HttpService:JSONEncode({
+				op = 1,
+				d = ""
+			}))
+		end
+	end)
+end
+
+function module.Update(name, typeo, details, state, assets)
+	assert(connection, "cant update if not started")
+	local payload = {
+		op = 3,
+		d = {
+			since = nil,
+			activities = {{
+				name = name or "no name",
+				type = typeo or 0,
+				details = details or "No Details!",
+				state = state or "No state!",
+				assets = module.assets or {},
+				timestamps = { start = os.time() * 1000 }
+			}},
+			status = "online",
+			afk = false
+		}
+	}
+	connection:Send(HttpService:JSONEncode(payload))
+end
+
+function module.Init(Token: string, name, typeo, details, state, assets)
+	assert(Token ~= "", "token must be provided")
+	
+	connection = ws.connect("wss://gateway.discord.gg/?v=10&encoding=json")
+	
+	if assets then
+		module.assets = assets
+	end
+	
+	connection.OnMessage:Connect(function(message)
+		local data = HttpService:JSONDecode(message)
+
+		if data.op == 10 then
+			local interval = data.d.heartbeat_interval
+			Heartbeat(interval)
+
+			connection:Send(HttpService:JSONEncode({
+				op = 2,
+				d = {
+					token = Token,
+					properties = {
+						["$os"] = "windows",
+						["$browser"] = "NullFire",
+						["$device"] = "NF"
+					},
+					presence = {
+						activities = {{
+							name = name or "no name",
+							type = typeo or 0,
+							details = details or "No Details!",
+							state = state or "No state!",
+							assets = module.assets or {},
+							timestamps = { start = os.time() * 1000 }
+						}},
+						status = "online",
+						afk = false
+					}
+				}
+			}))
+		end
+	end)
+end
+
+function module.Stop()
+	assert(connection, "cant stop if not even started!")
+	connection:Close()
+	connection = nil
+end
+
+return module
