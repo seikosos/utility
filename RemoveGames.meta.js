62e72bb9da5f01b8fcb9d7526b867f5d3622f662
seikoso <75853142+seikosos@users.noreply.github.com>
2026-02-19 15:44:29 +0100

    Update version number to 1.3

--- RemoveGames.meta.js
+++ RemoveGames.meta.js
@@ -3,7 +3,7 @@
 // @namespace    NullFire
 // @match        https://www.roblox.com/*
 // @grant        none
-// @version      1.2
+// @version      1.3
 // @author       seikoso
 // @downloadURL https://raw.githubusercontent.com/seikosos/utility/refs/heads/main/RemoveGames.user.js
 // @updateURL   https://raw.githubusercontent.com/seikosos/utility/refs/heads/main/RemoveGames.meta.js
