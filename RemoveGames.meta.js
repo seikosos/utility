22a5844fc84b459c9092989a76cad0d39f0df90e
seikoso <75853142+seikosos@users.noreply.github.com>
2026-02-19 15:39:03 +0100

    Create RemoveGames.meta.js

---
+++ RemoveGames.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Remove bad games from roblos
+// @namespace    NullFire
+// @match        https://www.roblox.com/*
+// @grant        none
+// @version      1.2
+// @author       seikoso
+// @downloadURL https://raw.githubusercontent.com/seikosos/utility/refs/heads/main/RemoveGames.user.js
+// @updateURL   https://raw.githubusercontent.com/seikosos/utility/refs/heads/main/RemoveGames.meta.js
+// ==/UserScript==
