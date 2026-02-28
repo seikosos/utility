d966908110a30c74d7089db6a5d129f3dc3a48b5
seikoso <75853142+seikosos@users.noreply.github.com>
2026-02-28 11:18:38 +0100

    Update version number to 1.6

--- RemoveGames.meta.js
+++ RemoveGames.meta.js
@@ -5,7 +5,7 @@
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        GM_registerMenuCommand
-// @version      1.5
+// @version      1.6
 // @author       seikoso
 // @downloadURL https://raw.githubusercontent.com/seikosos/utility/refs/heads/main/RemoveGames.user.js
 // @updateURL   https://raw.githubusercontent.com/seikosos/utility/refs/heads/main/RemoveGames.meta.js
